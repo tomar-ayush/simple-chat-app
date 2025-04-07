18d6d5b456c7667f02922095a799e66321f735ed
Ayush <ayushtomar8624@gmail.com>
2025-04-07 10:07:50 +0530

    feat: updating code

---
+++ src/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+	console.log('a user connected');
+
+	socket.on('disconnect', () => {
+		console.log('user disconnected');
+	});
+
+	socket.on('chat message', (msg) => {
+		io.emit('chat message', msg);
+	});
+});
+
+server.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
